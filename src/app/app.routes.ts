314ba4cb352ff420aa499db325747194f09a101a
Luna Nordbergh <dev@lunalu.org>
2025-05-07 00:48:14 +0200

    Localize text to Dutch
    Several UI elements, including button labels and descriptions, have been translated from English to Dutch for localization purposes. Google Analytics tracking code was also removed from the index.html file.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router';
 import { SocialsComponent } from './socials/socials.component';
 import { PronounsComponent } from './pronouns/pronouns.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
-import { NsfwComponent } from './nsfw/nsfw.component';
 
 export const routes: Routes = [
   { path: '', component: MainMenuComponent },
   { path: 'socials', component: SocialsComponent },
-  { path: 'pronouns', component: PronounsComponent },
-  { path: 'nsfw', component: NsfwComponent }
+  { path: 'pronouns', component: PronounsComponent }
 ];
