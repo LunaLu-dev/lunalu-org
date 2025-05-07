e3e7ecfe2b96372bf040aa7070e022c418982727
Luna Nordbergh <dev@lunalu.org>
2025-05-07 03:04:47 +0200

    Update responsive image handling for main component
    Replaced static image attributes with dynamic bindings to adjust image size and source based on screen width. Added logic in the component's TypeScript to manage dimensions and applied CSS for responsive styling, providing a better mobile and desktop user experience. Renamed and added optimized image assets.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {NgOptimizedImage} from '@angular/common';
 import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgOptimizedImage, FooterComponent],
+  imports: [RouterOutlet, NgOptimizedImage, FooterComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'lunalu-org';
+
+  imagePath: string = `/luna_300x300.webp`;
+
+  imgWidth: number = 300;
+  imgHeight: number = 300;
+
+  ngOnInit(): void {
+    this.setImgDim();
+    window.addEventListener('resize', () => this.setImgDim());
+  }
+
+  private setImgDim() {
+    const width = window.innerWidth;
+
+    //Mobile View
+    if (width < 600) {
+      this.imagePath = '/luna_200x200.webp';
+      this.imgWidth = 200;
+      this.imgHeight = 200;
+    } else {//Desktop View
+      this.imagePath = '/luna_300x300.webp';
+      this.imgWidth = 300;
+      this.imgHeight = 300;
+    }
+  }
 }
