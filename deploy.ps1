1a70f5165b9cc8b3e0d1156022236e84ca063189
luna <luna.lu@lunalu.dev>
2025-05-19 17:06:52 +0200

    Remove `package-lock.json`.
    This commit deletes the `package-lock.json` file, likely to avoid lockfile management issues or enforce consistency across environments. Ensure the removal aligns with project policies on dependency management.

---
+++ deploy.ps1
@@ -0,0 +1,19 @@
+if ($args[0] -eq "--test"){
+  ng build --configuration production
+  ssh -i "C:\Users\lunan\.ssh\SSH_KEY_NEW" -p 22 webserver@192.168.1.69 "rm -rf /home/webserver/webserver/web/testing.lunalu.org/www/dist"
+  scp -i "C:\Users\lunan\.ssh\SSH_KEY_NEW" -P 22 -r .\dist webserver@192.168.1.69:/home/webserver/webserver/web/testing.lunalu.org/www/
+  ssh -i "C:\Users\lunan\.ssh\SSH_KEY_NEW" -p 22 webserver@192.168.1.69 "chmod 777 -R /home/webserver/webserver/web/testing.lunalu.org/www/dist"
+  Write-Output "Done Webserver has been deployed to test server"
+}
+elseif ($args[0] -eq "--prod"){
+  ng build --configuration production
+  ssh -i "C:\Users\lunan\.ssh\SSH_KEY_NEW" -p 22 webserver@192.168.1.69 "rm -rf /home/webserver/webserver/web/lunalu.nl/www/dist"
+  scp -i "C:\Users\lunan\.ssh\SSH_KEY_NEW" -P 22 -r .\dist webserver@192.168.1.69:/home/webserver/webserver/web/lunalu.nl/www/
+  ssh -i "C:\Users\lunan\.ssh\SSH_KEY_NEW" -p 22 webserver@192.168.1.69 "chmod 777 -R /home/webserver/webserver/web/lunalu.nl/www/dist"
+  Write-Output "Done Webserver has been deployed to produsction server"
+}
+else {
+  Write-Output "---Invalid Agument---";
+  Write-Output "--test      |     Deploy to testing server"
+  Write-Output "--prod      |     Deploy to production server"
+}
--- src/app/socials/socials.component.ts
+++ src/app/socials/socials.component.ts
@@ -1,5 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
+import Analytics from 'analytics';
+// @ts-ignore
+import googleAnalytics from '@analytics/google-analytics'
+
+const analytics = Analytics({
+  app: 'lunalu.org',
+  plugins: [
+    googleAnalytics({
+      measurementIds: ['G-X5FND3X0TT']
+    })
+  ]
+})
 
 @Component({
   selector: 'app-socials',
@@ -10,27 +22,75 @@ import {NgOptimizedImage} from '@angular/common';
   styleUrl: './socials.component.css'
 })
 export class SocialsComponent {
-
-  //old link: https://only-fans.uk/LunaLu
-
-  links: string[] = [
-    "https://only-fans.uk/lunaluplayz",
-    "https://discord.com/users/1098996480011944017",
-    "https://www.twitch.tv/lunaluplayz",
-    "https://vrchat.com/home/user/usr_19d6e456-23a6-41d4-8d0a-cf0ddd305e47",
-    "https://www.reddit.com/user/LunaLu_04",
-    "https://github.com/LunaLu-dev",
-    "https://www.youtube.com/@Luna_Lu1",
-    "https://x.com/LunaLuplayz",
-    "https://www.planetminecraft.com/member/_luna04_/",
-    "https://www.pinterest.co.uk/lunaplayz04/",
-    "https://wishlist.lunalu.org/",
-    "https://steamcommunity.com/id/_LunaLu_/",
-    "https://open.spotify.com/user/31jo3k6nxhloleusg3tidy7ctimq?si=93424dcbf62e47ea",
-    "https://myanimelist.net/profile/LunaLuPlayz"
+  // Old Link: https://only-fans.uk/LunaLu
+  links: { name: string, url: string }[] = [
+    {
+      name: 'LunaLuPlayz',
+      url: 'https://only-fans.uk/lunaluplayz'
+    },
+    {
+      name: 'Discord',
+      url: 'https://discord.com/users/1098996480011944017'
+    },
+    {
+      name: 'Twitch',
+      url: 'https://www.twitch.tv/lunaluplayz'
+    },
+    {
+      name: 'VRChat',
+      url: 'https://vrchat.com/home/user/usr_19d6e456-23a6-41d4-8d0a-cf0ddd305e47'
+    },
+    {
+      name: 'Reddit',
+      url: 'https://www.reddit.com/user/LunaLu_04'
+    },
+    {
+      name: 'GitHub',
+      url: 'https://github.com/LunaLu-dev'
+    },
+    {
+      name: 'YouTube',
+      url: 'https://www.youtube.com/@Luna_Lu1'
+    },
+    {
+      name: 'X',
+      url: 'https://x.com/LunaLuplayz'
+    },
+    {
+      name: 'Planet Minecraft',
+      url: 'https://www.planetminecraft.com/member/_luna04_/'
+    },
+    {
+      name: 'Pinterest',
+      url: 'https://www.pinterest.co.uk/lunaplayz04/'
+    },
+    {
+      name: 'Wishlist',
+      url: 'https://wishlist.lunalu.org/'
+    },
+    {
+      name: 'Steam',
+      url: 'https://steamcommunity.com/id/_LunaLu_/'
+    },
+    {
+      name: 'Spotify',
+      url: 'https://open.spotify.com/user/31jo3k6nxhloleusg3tidy7ctimq?si=93424dcbf62e47ea'
+    },
+    {
+      name: 'MyAnimeList',
+      url: 'https://myanimelist.net/profile/LunaLuPlayz'
+    }
   ]
-  linkClick(index:number){
-    window.open(this.links[index]);
+
+
+  linkClick(index: number) {
+
+    analytics.track('SocialLinkClick', {
+      item: this.links[index].name,
+      url: this.links[index].url,
+      src: "lunalu.nl"
+    })
+    window.open(this.links[index].url, '_blank');
   }
 
   protected readonly window = window;
