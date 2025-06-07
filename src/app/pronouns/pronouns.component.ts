0d2d4fb832ca7c9d02cc3357abd81a936eeea22c
Luna Lu <104737060+LunaLu-dev@users.noreply.github.com>
2025-06-07 09:58:09 +0200

    Refactor "Personality Test" link: move to "About Me" section with Plausible tracking

--- src/app/pronouns/pronouns.component.ts
+++ src/app/pronouns/pronouns.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
+import Plausible from 'plausible-tracker'
+
+
+const {trackEvent} = Plausible({
+  domain: 'lunalu.org',
+  apiHost: 'https://analytics.lunalu.org'
+})
 
 @Component({
   selector: 'app-pronouns',
@@ -27,6 +34,28 @@ export class PronounsComponent {
     clearInterval(this.intervalId);
   }
 
+  links: {name: string, url: string}[] = [
+    {
+      name: 'Personality Test',
+      url: 'https://www.16personalities.com/profiles/infp-t/o/dhjxp3rqw'
+    }
+  ]
+
+  linkClick(index: number) {
+
+    trackEvent(
+      'social_link_click',
+      {
+        props: {
+          item: this.links[index].name,
+          url: this.links[index].url,
+          src: "luna.org"
+        }
+      }
+    );
+
+    window.open(this.links[index].url, '_blank');
+  }
   born:Date = new Date(2004, 4, 24);
   age:number = this.today.getFullYear() - this.born.getFullYear()
   protected readonly window = window;
