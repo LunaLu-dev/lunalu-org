283fc15d6c5b2ee04a996b48253d45a7330c5d13
Luna Lu <104737060+LunaLu-dev@users.noreply.github.com>
2026-01-29 13:26:00 +0100

    Fixed packages and updated to new link

--- src/app/socials/socials.component.ts
+++ src/app/socials/socials.component.ts
@@ -22,7 +22,7 @@ export class SocialsComponent {
   links: { name: string, url: string }[] = [
     {
       name: 'OnlyFans',
-      url: 'https://cdn.caponis.gay/rick.mp4'
+      url: 'https://cdn.gimmeboobs.com/rick.mp4'
     },
     {
       name: 'Discord',
